e7411215a6227e4126bf8e76a56067a6d20530d5
Joe Del Balzo <jdelbalzo99@gmail.com>
2023-08-10 13:25:54 -0400

    basic react-express template

--- server/app.js
+++ server/app.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-app.use(express.json());
+// use if you end up building out a back end
+// const express = require("express");
+// const path = require("path");
 
-app.use('/dist', express.static(path.join(__dirname, '../dist')));
-app.use('/static', express.static(path.join(__dirname, '../static')));
+// const app = express();
+// const port = process.env.PORT || 3000;
 
-app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../static/index.html')));
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/api/auth', require('./api/auth'));
-app.use('/api/orders', require('./api/orders'));
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
 
-module.exports = app;
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,25 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import auth from './auth';
-import cart from './cart';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import axios from "axios";
+
+const mainState = (state = [], action) => {
+  if (action.type === "SET_MAIN_STATE") {
+    return action.mainstate;
+  }
+  return state;
+};
 
 const reducer = combineReducers({
-  auth,
-  cart
+  mainState,
 });
 
+export const fetchMain = () => {
+  return async (dispatch) => {
+    const response = await axios.get("/");
+    dispatch({ type: "SET_MAIN_STATE", mainstate: response.data });
+  };
+};
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 export default store;
-
-export * from './auth';
-export * from './cart';
